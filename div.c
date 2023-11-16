c8834f8ddbbee9bfe782511360dbe996e02235eb
youssef aitbenaddi <youssefaitbenaddi8@gmail.com>
2023-11-16 09:45:37 -0800

    f

--- div.c
+++ div.c
@@ -1,4 +1,5 @@
 #include "mine.h"
+
 /**
  * div_func - Divides the second top element by the top element of the stack
  * @stack: A pointer to the head of the stack
@@ -8,6 +9,7 @@ void div_func(my_stack_t **stack, unsigned int line_number)
 {
 	my_stack_t *second_top = (*stack)->next;
 	int top_value = (*stack)->n;
+
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
