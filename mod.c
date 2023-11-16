b369ca8062a7d6d7697529e92eea67761ae16ed0
youssef aitbenaddi <youssefaitbenaddi8@gmail.com>
2023-11-16 09:50:10 -0800

    f

--- mod.c
+++ mod.c
@@ -1,6 +1,5 @@
 #include "mine.h"
 #include <stdio.h>
-
 /**
 * mod - mod operations
 * @stack: A pointer to the head of the stack
@@ -8,23 +7,18 @@
 */
 void mod(my_stack_t **stack, unsigned int line_number)
 {
-
 my_stack_t *top = *stack;
 *stack = top->next;
-
 if (!stack || !*stack || !(*stack)->next)
 {
 	fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
 	exit(EXIT_FAILURE);
 }
-
 if (top->n == 0)
 {
 	fprintf(stderr, "L%d: division by zero\n", line_number);
 	exit(EXIT_FAILURE);
 }
-
 (*stack)->n %= top->n;
 free(top);
 }
-
