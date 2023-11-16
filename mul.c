b32abcef5bf14d46bf21cdf64b7e28497196d913
youssef aitbenaddi <youssefaitbenaddi8@gmail.com>
2023-11-16 09:48:17 -0800

    f

--- mul.c
+++ mul.c
@@ -1,5 +1,4 @@
 #include "mine.h"
-
 /**
 * mul - Multiplies the second top element of the stack by the top element
 * @stack: A pointer to the head of the stack
@@ -9,14 +8,11 @@ void mul(my_stack_t **stack, unsigned int line_number)
 {
 my_stack_t *top = *stack;
 *stack = top->next;
-
 if (!stack || !*stack || !(*stack)->next)
 {
 	fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 	exit(EXIT_FAILURE);
 }
-
 (*stack)->n *= top->n;
 free(top);
 }
-
